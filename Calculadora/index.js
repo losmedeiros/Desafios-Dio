c73f30cb2c0127a2aceaec37ef2de4f5b4565be5
losmedeiros <losmedeiros@gmail.com>
2023-10-24 02:00:38 -0300

    Quarto commit final envio

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -1,9 +1,28 @@
-let nunVitorias=10;
-let nunDerrotas=5;
+let vitorias=100;
+let derrotas=5;
 let total;
-function somarGanhos(nunVitoria,nunDerrota){
+let nivelHeroi="";
+function calculoVitorias(nunVitoria,nunDerrota){
   total=nunVitoria - nunDerrota;
   return total;
 }
-let ranquear = somarGanhos
-console.log("o numero do ranque é", ranquear)
+let saldoVitorias = calculoVitorias(vitorias,derrotas)
+
+if(saldoVitorias<=10){
+ nivelHeroi="ferro"
+}else if(saldoVitorias>10&&saldoVitorias<=20){
+ nivelHeroi="Bronze"                               
+}else if(saldoVitorias>20&&saldoVitorias<=50){
+ nivelHeroi="Prata"
+}else if(saldoVitorias>50&&saldoVitorias<=80){
+ nivelHeroi="Ouro"
+}else if(saldoVitorias>80&&saldoVitorias<=90){
+ nivelHeroi="Diamante"
+}else if(saldoVitorias>90&&saldoVitorias<=100){
+ nivelHeroi="Lendário"
+}else if(saldoVitorias>100){
+ nivelHeroi="Imortal"
+}
+//Ao final deve se exibir uma mensagem:
+//"O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivelHeroi}`)
